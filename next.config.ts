cfd82d6253145a1e242af654f1fea3d9443c3ae5
mehedi259 <mmridul207@gmail.com>
2026-07-21 06:16:46 +0600

    fix: proxy API requests to bypass mixed content

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://188.245.212.240/api/:path*', // Proxy to Hetzner Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
